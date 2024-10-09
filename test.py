693a99bd8f55aaa95ca1728d0fc32f087d01ad9d
Chanpreet Singh <chanpreet3000@gmail.com>
2024-10-09 12:33:26 +0530

    temp

--- test.py
+++ test.py
@@ -25,12 +25,12 @@ headers = {
     'referer': 'https://www.theperfumeshop.com/',
     'sec-ch-ua': '"Brave";v="129", "Not=A?Brand";v="8", "Chromium";v="129"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Windows"',
+    'sec-ch-ua-platform': '"Mac"',
     'sec-fetch-dest': 'empty',
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-site',
     'sec-gpc': '1',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 9_8_8; en-US) AppleWebKit/601.29 (KHTML, like Gecko) Chrome/53.0.3214.218 Safari/536',
     'x-anonymous-consents': '%5B%5D',
 }
 
